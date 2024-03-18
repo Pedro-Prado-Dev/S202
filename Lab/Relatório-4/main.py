a83ac65cb16075dfc3df82dca18b83a253ff53f2
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-03-18 19:18:07 -0300

    :white_check_mark: main: using new functionalities

--- Lab/Relatório-4/main.py
+++ Lab/Relatório-4/main.py
@@ -1,5 +1,20 @@
 from database import Database
-from writeAJson import writeAJson
+from helper.write_a_json import writeAJson
+from product_analyzer import ProductAnalyzer
 
 db = Database(database="mercado", collection="produtos")
-db.resetDatabase()
+#db.resetDatabase()
+
+product_analyzer = ProductAnalyzer()
+
+total_sales_per_day = product_analyzer.total_sales_per_day()
+writeAJson(total_sales_per_day, "Total de vendas por dia")
+
+most_sold_product = product_analyzer.most_sold_product()
+writeAJson(most_sold_product, "Produto mais vendido")
+
+customer_highest_spending = product_analyzer.customer_highest_spending()
+writeAJson(customer_highest_spending, "Cliente que mais gastou em uma única compra")
+
+products_sold_above_quantity = product_analyzer.products_sold_above_quantity()
+writeAJson(products_sold_above_quantity, "Produtos vendidos acima de 1 unidade")
